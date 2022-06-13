f8f4c88ca7691973038d90c843dfc879249dfd83
Phil Eagleson <peagleson@symitar.com>
2022-06-13 16:59:58 -0400

    added lspconfig for emmet_ls

--- nvim/.config/nvim/lua/phil/lsp.lua
+++ nvim/.config/nvim/lua/phil/lsp.lua
@@ -1,19 +1,19 @@
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 local on_attach = function()
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer=0 })
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer=0 })
-    vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, { buffer=0 })
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { buffer=0 })
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, { buffer=0 })
-    vim.keymap.set('n', '<leader>dn', vim.diagnostic.goto_next, { buffer=0 })
-    vim.keymap.set('n', '<leader>dp', vim.diagnostic.goto_prev, { buffer=0 })
-    vim.keymap.set('n', '<leader>dl', '<cmd>Telescope diagnostics<cr>' , { buffer=0 })
-    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { buffer=0 })
-    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { buffer=0 })
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = 0 })
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = 0 })
+    vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, { buffer = 0 })
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { buffer = 0 })
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, { buffer = 0 })
+    vim.keymap.set('n', '<leader>dn', vim.diagnostic.goto_next, { buffer = 0 })
+    vim.keymap.set('n', '<leader>dp', vim.diagnostic.goto_prev, { buffer = 0 })
+    vim.keymap.set('n', '<leader>dl', '<cmd>Telescope diagnostics<cr>', { buffer = 0 })
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { buffer = 0 })
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { buffer = 0 })
 end
 
-require'nvim-lsp-installer'.setup({
+require 'nvim-lsp-installer'.setup({
     automatic_installation = true,
     ui = {
         icons = {
@@ -24,14 +24,19 @@ require'nvim-lsp-installer'.setup({
     }
 })
 
-
-require'lspconfig'.gopls.setup{
+require 'lspconfig'.emmet_ls.setup {
     capabilities = capabilities,
     on_attach = on_attach,
 }
 
 
-require'lspconfig'.sumneko_lua.setup{
+require 'lspconfig'.gopls.setup {
+    capabilities = capabilities,
+    on_attach = on_attach,
+}
+
+
+require 'lspconfig'.sumneko_lua.setup {
     capabilities = capabilities,
     on_attach = on_attach,
     settings = {
@@ -51,5 +56,3 @@ require'lspconfig'.sumneko_lua.setup{
         }
     }
 }
-
-
