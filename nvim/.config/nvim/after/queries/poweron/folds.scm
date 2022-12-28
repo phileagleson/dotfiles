e0956c0ce32d670acd44e8619ac940701dcdcc27
Phil Eagleson <peagleson@symitar.com>
2022-12-28 17:09:33 -0500

    added auto-folds to poweron files

---
+++ nvim/.config/nvim/after/queries/poweron/folds.scm
@@ -0,0 +1,11 @@
+[
+ (procedure_definition)
+ (define_division)
+ (setup_division)
+ (print_division)
+ (sort_division)
+ (total_division)
+ (for_statement)
+ (if_statement)
+ (while_statement)
+] @fold
--- nvim/.config/nvim/lua/phil/autocommands.lua
+++ nvim/.config/nvim/lua/phil/autocommands.lua
@@ -12,9 +12,23 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 
 local indentGroup = vim.api.nvim_create_augroup('PoweronIndents', { clear = true })
 vim.api.nvim_create_autocmd('FileType poweron', {
+  buffer = 0,
   callback = function()
     vim.opt_local.shiftwidth = 1
     vim.opt_local.tabstop = 1
+    vim.opt.foldmethod = 'expr'
+    vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+    vim.opt.foldcolumn = '2'
+    require('capslock').toggle('n')
   end,
   group = indentGroup,
+})
+
+local foldGroup = vim.api.nvim_create_augroup('PoweronFolds', { clear = true })
+vim.api.nvim_create_autocmd({ 'FileType poweron', 'BufReadPost,FileReadPost' }, {
+  buffer = 0,
+  callback = function()
+    vim.api.nvim_command('normal zR')
+  end,
+  group = foldGroup,
 })
