0cdbed4af52f2ed83d1b443f4c262f7a2453bd8b
Phil Eagleson <peagleson@symitar.com>
2023-01-05 17:08:37 -0500

    configured dap go

--- nvim/.config/nvim/after/plugin/dap.lua
+++ nvim/.config/nvim/after/plugin/dap.lua
@@ -33,56 +33,42 @@ dap.listeners.before.event_exited["dapui_config"] = function()
 end
 
 require 'mason-nvim-dap'.setup {
-  ensure_installed = { 'node2', 'chrome', },
+  ensure_installed = { 'node2', 'chrome', 'delve' },
   automatic_setup = true,
 }
 
--- dap.adapters.go = {
---   type = "executable",
---   command = "/home/phil/go/bin/dlv",
---   args = {},
--- }
+dap.set_log_level('TRACE')
 
--- dap.configurations.go = {
---   {
---     type = "go",
---     request = "launch",
---     name = "Launch file",
---     program = "${file}"
---   }
--- }
-
--- dap.adapters.delve = {
---   type = 'server',
---   port = '${port}',
---   executable = {
---     command = '/home/phil/go/bin/dlv',
---     args = { 'dap', '-l', '127.0.0.1:${port}' },
---   }
--- }
+dap.adapters.delve = {
+  type = 'server',
+  port = '${port}',
+  executable = {
+    command = 'dlv',
+    args = { 'dap', '-l', '127.0.0.1:${port}' },
+  }
+}
 
 -- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
--- dap.configurations.go = {
---   {
---     type = "delve",
---     name = "Debug",
---     request = "launch",
---     program = "${file}"
---   },
---   {
---     type = "delve",
---     name = "Debug test", -- configuration for debugging test files
---     request = "launch",
---     mode = "test",
---     program = "${file}"
---   },
---   -- works with go.mod packages and sub packages
---   {
---     type = "delve",
---     name = "Debug test (go.mod)",
---     request = "launch",
---     mode = "test",
---     program = "./${relativeFileDirname}"
---   }
--- }
--- dap.set_log_level('TRACE')
+dap.configurations.go = {
+  {
+    type = "delve",
+    name = "Debug",
+    request = "launch",
+    program = "${workspaceFolder}"
+  },
+  {
+    type = "delve",
+    name = "Debug test", -- configuration for debugging test files
+    request = "launch",
+    mode = "test",
+    program = "${file}"
+  },
+  -- works with go.mod packages and sub packages
+  {
+    type = "delve",
+    name = "Debug test (go.mod)",
+    request = "launch",
+    mode = "test",
+    program = "./${relativeFileDirname}"
+  }
+}
--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -1,23 +1,23 @@
 local mason_null_ls_status, mason_null_ls = pcall(require, 'mason-null-ls')
 local handlers = require 'vim.lsp.handlers'
-local popup = require('plenary.popup')
+--local popup = require('plenary.popup')
 
 if not mason_null_ls_status then
- print("Error loading mason-null-ls.. is it installed?")
+  print("Error loading mason-null-ls.. is it installed?")
 end
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 local on_attach = function()
- vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = 0 })
- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = 0 })
- vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<CR>', { buffer = 0 })
- vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { buffer = 0 })
- vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { buffer = 0 })
- vim.keymap.set('n', '<leader>dn', vim.diagnostic.goto_next, { buffer = 0 })
- vim.keymap.set('n', '<leader>dp', vim.diagnostic.goto_prev, { buffer = 0 })
- vim.keymap.set('n', '<leader>ds', '<cmd>Telescope lsp_document_symbols<cr>', { buffer = 0 })
- vim.keymap.set('n', '<leader>dl', '<cmd>Telescope diagnostics<cr>', { buffer = 0 })
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = 0 })
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = 0 })
+  vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<CR>', { buffer = 0 })
+  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { buffer = 0 })
+  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { buffer = 0 })
+  vim.keymap.set('n', '<leader>dn', vim.diagnostic.goto_next, { buffer = 0 })
+  vim.keymap.set('n', '<leader>dp', vim.diagnostic.goto_prev, { buffer = 0 })
+  vim.keymap.set('n', '<leader>ds', '<cmd>Telescope lsp_document_symbols<cr>', { buffer = 0 })
+  vim.keymap.set('n', '<leader>dl', '<cmd>Telescope diagnostics<cr>', { buffer = 0 })
 end
 
 -- must setup in order
@@ -26,189 +26,189 @@ end
 -- 3. lspconfig
 --
 require 'mason'.setup({
- max_concurrent_installers = 4,
- ui = {
-  icons = {
-   server_installed = "✓",
-   server_pending = "➜",
-   server_uninstalled = "✗"
+  max_concurrent_installers = 4,
+  ui = {
+    icons = {
+      server_installed = "✓",
+      server_pending = "➜",
+      server_uninstalled = "✗"
+    }
   }
- }
 })
 
 require 'mason-lspconfig'.setup {
- ensure_installed = {},
- automatic_installation = false,
+  ensure_installed = {},
+  automatic_installation = false,
 }
 local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
 mason_null_ls.setup {
- ensure_installed = {},
- on_attach = function(current_client, bufnr)
-  if current_client.supports_method('textDocument/formatting') then
-   vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-   vim.api.nvim_create_autocmd('BufWritePre', {
-    group = augroup,
-    buffer = bufnr,
-    callback = function()
-     vim.lsp.buf.format({
-      filter = function(client)
-       -- only use null-ls for formatting instead fo lsp server
-       return client.name == 'null-ls'
-      end,
-      buffnr = bufnr,
-     })
-    end,
-   })
+  ensure_installed = {},
+  on_attach = function(current_client, bufnr)
+    if current_client.supports_method('textDocument/formatting') then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd('BufWritePre', {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({
+            filter = function(client)
+              -- only use null-ls for formatting instead fo lsp server
+              return client.name == 'null-ls'
+            end,
+            buffnr = bufnr,
+          })
+        end,
+      })
+    end
   end
- end
 }
 require 'lspconfig'.emmet_ls.setup {
- capabilities = capabilities,
- on_attach = on_attach,
+  capabilities = capabilities,
+  on_attach = on_attach,
 }
 
 
 require 'lspconfig'.gopls.setup {
- capabilities = capabilities,
- on_attach = on_attach,
+  capabilities = capabilities,
+  on_attach = on_attach,
 }
 
 require 'lspconfig'.clangd.setup {
- capabilities = capabilities,
- on_attach = on_attach,
+  capabilities = capabilities,
+  on_attach = on_attach,
 }
 
 require 'lspconfig'.clangd.setup {
- capabilities = capabilities,
- on_attach = on_attach,
+  capabilities = capabilities,
+  on_attach = on_attach,
 }
 
 require 'lspconfig'.tsserver.setup {
- capabilities = capabilities,
- on_attach = on_attach,
+  capabilities = capabilities,
+  on_attach = on_attach,
 }
 
 require 'lspconfig'.sumneko_lua.setup {
- capabilities = capabilities,
- on_attach = on_attach,
- settings = {
-  Lua = {
-   diagnostics = {
-    globals = { 'vim', 'packer_bootstrap' }
-   },
-   workspace = {
-    library = {
-     [vim.fn.expand "$VIMRUNTIME/lua"] = true,
-     [vim.fn.stdpath "config" .. "/lua"] = true,
-    },
-   },
-   telemetry = {
-    enable = false,
-   },
+  capabilities = capabilities,
+  on_attach = on_attach,
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { 'vim', 'packer_bootstrap' }
+      },
+      workspace = {
+        library = {
+          [vim.fn.expand "$VIMRUNTIME/lua"] = true,
+          [vim.fn.stdpath "config" .. "/lua"] = true,
+        },
+      },
+      telemetry = {
+        enable = false,
+      },
+    }
   }
- }
 }
 
-local lsp_flags = {
- debounce_text_changes = 150
-}
+-- local lsp_flags = {
+--  debounce_text_changes = 150
+-- }
 local configs = require "lspconfig.configs"
 local util = require 'lspconfig.util'
 local os = vim.loop.os_uname().sysname
 local cmd = { "" }
 local commonDir = ''
 if os == "Darwin" then
- cmd = { "node", "/Users/phil/projects/poweron-language-server/out/main.js", "--stdio" }
- --commonDir = '/Users/phil/projects/poweron/RDFILES'
- require 'lspconfig'.sumneko_lua.setup {
-  capabilities = capabilities,
-  on_attach = on_attach,
-  settings = {
-   Lua = {
-    diagnostics = {
-     globals = { 'vim', 'packer_bootstrap' }
-    },
-    workspace = {
-     library = {
-      [vim.fn.expand "$VIMRUNTIME/lua"] = true,
-      [vim.fn.stdpath "config" .. "/lua"] = true,
-     },
-    },
-    telemetry = {
-     enable = false,
-    },
-   }
+  cmd = { "node", "/Users/phil/projects/poweron-language-server/out/main.js", "--stdio" }
+  --commonDir = '/Users/phil/projects/poweron/RDFILES'
+  require 'lspconfig'.sumneko_lua.setup {
+    capabilities = capabilities,
+    on_attach = on_attach,
+    settings = {
+      Lua = {
+        diagnostics = {
+          globals = { 'vim', 'packer_bootstrap' }
+        },
+        workspace = {
+          library = {
+            [vim.fn.expand "$VIMRUNTIME/lua"] = true,
+            [vim.fn.stdpath "config" .. "/lua"] = true,
+          },
+        },
+        telemetry = {
+          enable = false,
+        },
+      }
+    }
   }
- }
 else
- cmd = { "node", "/home/phil/desktop/poweron-language-server/out/main.js", "--stdio" }
- --commonDir = '/home/phil/desktop/poweron/commonFiles/'
+  cmd = { "node", "/home/phil/desktop/poweron-language-server/out/main.js", "--stdio" }
+  --commonDir = '/home/phil/desktop/poweron/commonFiles/'
 end
 
 local function on_workspace_exec_command(err, actions, ctx)
- if (ctx.params.command == 'poweronlsp.showDataTypeNotification') then
-  local uri     = ctx['params']['arguments'][1]['uri']
-  local varName = ctx['params']['arguments'][1]['varName']
-  local bufnr   = ctx.bufnr
-  vim.ui.select({
-   "CHARACTER",
-   "CODE",
-   "DATE",
-   "FLOAT",
-   "MONEY",
-   "NUMBER",
-   "RATE"
-  }, {
-   prompt = "Choose Data type"
-  }, function(choice)
-   if (choice) then
-    vim.lsp.buf_request(bufnr, 'workspace/executeCommand', {
-     command = 'lsp.addVarToDefine',
-     arguments = {
-      {
-       uri = uri,
-       varName = varName,
-       dataType = choice
-      }
-     }
-    }, nil)
-   end
-  end)
- end
+  if (ctx.params.command == 'poweronlsp.showDataTypeNotification') then
+    local uri     = ctx['params']['arguments'][1]['uri']
+    local varName = ctx['params']['arguments'][1]['varName']
+    local bufnr   = ctx.bufnr
+    vim.ui.select({
+      "CHARACTER",
+      "CODE",
+      "DATE",
+      "FLOAT",
+      "MONEY",
+      "NUMBER",
+      "RATE"
+    }, {
+      prompt = "Choose Data type"
+    }, function(choice)
+      if (choice) then
+        vim.lsp.buf_request(bufnr, 'workspace/executeCommand', {
+          command = 'lsp.addVarToDefine',
+          arguments = {
+            {
+              uri = uri,
+              varName = varName,
+              dataType = choice
+            }
+          }
+        }, nil)
+      end
+    end)
+  end
 
- handlers[ctx.method](err, actions, ctx)
+  handlers[ctx.method](err, actions, ctx)
 end
 
 configs["poweronls"] = {
- default_config = {
-  cmd = cmd,
-  root_dir = util.find_git_ancestor,
-  filetypes = { "poweron" },
-  autostart = true,
-  handlers = {
-   ['workspace/executeCommand'] = on_workspace_exec_command
-  }
- },
+  default_config = {
+    cmd = cmd,
+    root_dir = util.find_git_ancestor,
+    filetypes = { "poweron" },
+    autostart = true,
+    handlers = {
+      ['workspace/executeCommand'] = on_workspace_exec_command
+    }
+  },
 }
 
 require 'lspconfig'["poweronls"].setup {
- on_attach = on_attach,
- --flags = lsp_flags,
- capabilities = capabilities,
- settings = {
-  commonFilesDirectory = commonDir
- },
+  on_attach = on_attach,
+  --flags = lsp_flags,
+  capabilities = capabilities,
+  settings = {
+    commonFilesDirectory = commonDir
+  },
 }
 
 --vim.lsp.set_log_level("debug")
 local setup, null_ls = pcall(require, 'null-ls')
 if not setup then
- return
+  return
 end
 
 -- local formatting = null_ls.builtins.formatting
 -- local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup {
- sources = {}
+  sources = {}
 }
