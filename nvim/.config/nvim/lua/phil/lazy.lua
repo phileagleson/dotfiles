35a4c177f60bc25348e215eeee258856ba4b67ca
Phil Eagleson <phileagleson1@gmail.com>
2023-07-21 16:33:42 -0400

    Updated some stuff and added rofi

--- nvim/.config/nvim/lua/phil/lazy.lua
+++ nvim/.config/nvim/lua/phil/lazy.lua
@@ -57,6 +57,20 @@ require('lazy').setup({
     vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
     end
   },
+  --[[ {
+    "jackMort/ChatGPT.nvim",
+      event = "VeryLazy",
+      dependencies = {
+        "MunifTanjim/nui.nvim",
+        "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope.nvim",
+      },
+      config = function()
+        require("chatgpt").setup({
+          api_key_cmd = "pass show chatgptapi",
+      })
+      end,
+  }, ]]
 
   'mbbill/undotree',
 
