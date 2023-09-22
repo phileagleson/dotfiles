0939d290ee4de26671546573b0367587b2108017
Phil Eagleson <peagleson@symitar.com>
2023-09-22 16:17:13 -0400

    Removed extra lazy-lock files

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -145,7 +145,8 @@ local cmd = { "" }
 local commonDir = ''
 local sysname = os.getenv("SYSNAME")
 if (sysname == "archie") then 
-  cmd = { "/home/phil/projects/pols/bin/pols" }
+  --cmd = { "/home/phil/projects/pols/bin/pols" }
+  cmd = { "node", "/mnt/c/Users/peagleson/Desktop/poweron-language-server/out/main.js", "--stdio" }
   --commonDir = '/Users/phil/projects/poweron/RDFILES'
   require 'lspconfig'.luau_lsp.setup {
     capabilities = capabilities,
@@ -153,7 +154,7 @@ if (sysname == "archie") then
     settings = {
       Lua = {
         diagnostics = {
-          globals = { 'vim', 'packer_bootstrap' }
+          --globals = { 'vim', 'packer_bootstrap' }
         },
         workspace = {
           library = {
