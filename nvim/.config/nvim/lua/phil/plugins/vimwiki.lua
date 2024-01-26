45d3b59a4fbe860015efa2079d359de62519d037
Phil Eagleson <phileagleson1@gmail.com>
2024-01-26 09:48:19 -0500

    updated some settings

--- nvim/.config/nvim/lua/phil/plugins/vimwiki.lua
+++ nvim/.config/nvim/lua/phil/plugins/vimwiki.lua
@@ -1,12 +1,16 @@
 return {
-	"vimwiki/vimwiki",
-	init = function()
-		vim.g.vimwiki_list = {
-			{
-				path = "~/vimwiki",
-				syntax = "markdown",
-				ext = ".md",
-			},
-		}
-	end,
+    "vimwiki/vimwiki",
+    init = function()
+        vim.g.vimwiki_list = {
+            {
+                path = "~/vimwiki",
+                syntax = "markdown",
+                ext = ".md",
+            },
+        }
+    end,
+    config = function()
+        vim.keymap.set("n", "<leader><CR>", vim.cmd.VimwikiVSplitLink)
+    end,
+    --<Plug>VimwikiVSplitLink
 }
